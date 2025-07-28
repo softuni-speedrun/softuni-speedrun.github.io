05958f66490778a18fc796d186a94498c1ce74cb
Viktor Kostadinov <viktor.pts@abv.bg>
2025-07-28 18:58:06 +0300

    add SPA support for github pages

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import page from 'https://unpkg.com/page@1.11.6/page.mjs';
+
 import * as api from './data/books.js';
 import * as comment from './data/comments.js';
 import * as userApi from './data/users.js';
@@ -5,3 +7,8 @@ import * as userApi from './data/users.js';
 window.api = api;
 window.userApi = userApi;
 window.comment = comment;
+
+page('/', () => console.log('home'));
+page('/about', () => console.log('about'));
+
+page.start();
